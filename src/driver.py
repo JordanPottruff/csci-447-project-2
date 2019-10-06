8f0c96dcef1c3b33e59e04e22ed84498342001c5
Jordan Pottruff <jordanpottruff@gmail.com>
2019-10-06 17:13:46 -0600

    Add start of k-means centroid calculation.

--- src/driver.py
+++ src/driver.py
@@ -1,13 +1,15 @@
 
 import src.datasets.data_set as ds
+import src.algorithms.k_means as kmeans
 
 
 def get_abalone_data():
-    abalone_data = ds.DataSet("../data/abalone.data", 9, list(range(0, 9)))
+    abalone_data = ds.DataSet("../data/abalone.data", 8, list(range(0, 8)))
+    numeric_columns = list(range(1, 8))
     # Convert attribute columns to floats
-    abalone_data.convert_to_float(abalone_data.attr_cols)
+    abalone_data.convert_to_float(numeric_columns)
     # Normalize values
-    abalone_data.normalize_z_score(list(range(1, 9)))
+    abalone_data.normalize_z_score(numeric_columns)
     return abalone_data
 
 
@@ -20,25 +22,32 @@ def get_car_data():
     car_data.convert_attribute(3, {'2': 2, '4': 4, 'more': 5})
     car_data.convert_attribute(4, {'small': 0, 'med': 1, 'big': 2})
     car_data.convert_attribute(5, {'low': 0, 'med': 1, 'high': 2})
+    numeric_columns = list(range(0, 6))
     # Normalize values.
-    car_data.normalize_z_score(list(range(0, 6)))
+    car_data.normalize_z_score(numeric_columns)
     return car_data
 
 
 def get_forest_fires_data():
     forest_fires_data = ds.DataSet("../data/forestfires.data", 12, list(range(0, 12)))
+    numeric_columns = [0, 1] + list(range(4, 13))
     # Remove the first line, which is the header info.
     forest_fires_data.remove_header()
-    # Convert all columns to floats, including the class column.
-    forest_fires_data.convert_to_float(list(range(0, 13)))
+    # Convert applicable columns to floats, including the class column.
+    forest_fires_data.convert_to_float(numeric_columns)
+    # Normalize values.
+    forest_fires_data.normalize_z_score(numeric_columns)
     return forest_fires_data
 
 
 def get_machine_data():
     # There is another final column but we probably want to exclude it.
     machine_data = ds.DataSet("../data/machine.data", 8, list(range(0, 8)))
+    numeric_columns = list(range(2, 9))
     # Convert all columns except the first two to floats, including the class column.
-    machine_data.convert_to_float(list(range(2, 9)))
+    machine_data.convert_to_float(numeric_columns)
+    # Normalize values.
+    machine_data.normalize_z_score(numeric_columns)
     return machine_data
 
 
@@ -47,5 +56,10 @@ def main():
     car_data = get_car_data()
     forest_fires_data = get_forest_fires_data()
     machine_data = get_machine_data()
+
+    km = kmeans.KMeans(machine_data, 2)
+    print(km.centroids)
+
+
 
 main()
