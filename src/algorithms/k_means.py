8676439319c17d00e284dbc123be4cbf7d7e593c
Jordan Pottruff <jordanpottruff@gmail.com>
2019-10-07 12:37:23 -0600

    Clean up k_means and add a testing setup.

--- src/algorithms/k_means.py
+++ src/algorithms/k_means.py
@@ -6,85 +6,116 @@ class KMeans:
     def __init__(self, training_data, k):
         self.training_data = training_data
         self.k = k
-        self.clusters = [[] for i in range(k)]
-        self.centroids = self.calculate_centroids()
+        self.centroids, self.clusters = self.calculate_clusters()
 
-    def calculate_centroids(self):
-        self.generate_random_centroids()
+    def calculate_clusters(self):
+        # First, generate centroids randomly
+        centroids = self.generate_random_centroids()
+        clusters = []
 
+        # TODO(jordan): will need to remove this n variable and instead evaluate whether the clusters have changed.
         n = 100
-        while (n > 0):
-            self.clusters = [[] for i in range(self.k)]
+        while n > 0:
+            # Reset the clusters to have 0 observations.
+            clusters = [[] for i in range(self.k)]
+
+            # Assign each observation to the cluster corresponding to the closest centroid.
             for obs in self.training_data.get_data():
-                min_centroid_i = None
-                min_dist = None
-                for i in range(self.k):
-                    centroid = self.centroids[i]
-                    dist = self.training_data.distance(centroid, obs)
-                    if min_centroid_i is None or dist < min_dist:
-                        min_centroid_i = i
-                        min_dist = dist
-                self.clusters[min_centroid_i].append(obs)
+                closest_centroid_i = self.find_closest_centroid(obs, centroids)
+                clusters[closest_centroid_i].append(obs)
 
+            # With all observations assigned to a cluster, we now recalculate the centroids as the cluster means.
             for i in range(self.k):
-                self.centroids[i] = self.calculate_cluster_mean(self.clusters[i])
+                centroids[i] = self.calculate_cluster_mean(clusters[i])
 
-            num_observations_per_cluster = []
-            for cluster in self.clusters:
-                num_observations_per_cluster.append(len(cluster))
-            print("Observations per cluster: ")
-            print(num_observations_per_cluster)
-            print()
             n -= 1
 
-        return self.centroids
+        return centroids, clusters
 
+    # Creates a list of centroids that are "random." A centroid is really just a vector of attribute values, so we
+    # accomplish this by randomly selecting an attribute value from our training data. Returns the centroids as a 2D
+    # list.
     def generate_random_centroids(self):
         num_training_data = len(self.training_data.get_data())
         num_cols = len(self.training_data.get_data()[0])
-        self.centroids = [[] for i in range(self.k)]
+        centroids = [[] for i in range(self.k)]
 
         # Initialize the means using randomly selected attribute values.
         for i in range(self.k):
-            self.centroids[i] = [None for i in range(num_cols)]
+            centroids[i] = [None for i in range(num_cols)]
             for attr_col in self.training_data.attr_cols:
                 selected_obs = self.training_data.get_data()[rand.randrange(0, num_training_data)]
-                self.centroids[i][attr_col] = selected_obs[attr_col]
+                centroids[i][attr_col] = selected_obs[attr_col]
+        return centroids
 
+    # Out of the list of all centroids, this will return the corresponding index for the centroid that is closest to the
+    # given observation.
+    def find_closest_centroid(self, obs, centroids):
+        # Index of the closest centroid seen so far.
+        closest_centroid_i = None
+        # Distance to the closest centroid seen so far.
+        min_dist = None
+        for i in range(len(centroids)):
+            dist = self.training_data.distance(centroids[i], obs)
+            if closest_centroid_i is None or dist < min_dist:
+                closest_centroid_i = i
+                min_dist = dist
+        return closest_centroid_i
+
+    # Calculates the mean of the cluster. This is tricky because not all attributes are numeric -- some are strings.
+    # When we encounter a string attribute, we simply calculate the mode (using a frequency dictionary) and use that
+    # as the "mean".
     def calculate_cluster_mean(self, cluster):
         num_cols = len(self.training_data.get_data()[0])
         attr_cols = self.training_data.attr_cols
         str_attr_cols = self.training_data.get_str_attr_cols()
-        n = len(cluster)
 
+        # The sums and freqs lists are used for calculating the average and mode of numeric and string attributes,
+        # respectively. Both have the same number of columns as our observations, but only the columns corresponding
+        # to string (freqs[i]) or numeric (sums[i]) will be filled in.
         sums = [0 for i in range(num_cols)]
         freqs = [{} for i in range(num_cols)]
 
+        # This section does one of two things:
+        # (1) calculates the sum of numeric attributes (and stores it in the corresponding columns of 'sums'),
+        # (2) calculates the frequency of values in string attributes (and stores it in a dictionary in the freqs list).
         for obs in cluster:
-            for col in range(num_cols):
-                if col in attr_cols:
-                    val = obs[col]
-                    if col in str_attr_cols:
-                        if val in freqs[col]:
-                            freqs[col][val] += 1
-                        else:
-                            freqs[col][val] = 1
+            # We iterate over the attribute columns only (to ignore class and unused columns)
+            for attr_col in attr_cols:
+                # If our attribute column is string-valued...
+                if attr_col in str_attr_cols:
+                    # ...then update the frequency table by...
+                    str_val = obs[attr_col]
+                    if str_val in freqs[attr_col]:
+                        # ...incrementing an existing count, if present, or
+                        freqs[attr_col][str_val] += 1
                     else:
-                        sums[col] += obs[col]
-
-        centroid = [0 for i in range(num_cols)]
-        for i in range(len(attr_cols)):
-            if i in attr_cols:
-                if i in str_attr_cols:
-                    freq = freqs[i]
-                    max_attr_val = None
-                    for attr_val in freq:
-                        if max_attr_val is None or freq[attr_val] > freq[max_attr_val]:
-                            max_attr_val = attr_val
-                    centroid[i] = max_attr_val
+                        # ...setting the count equal to 1 if value not previously seen.
+                        freqs[attr_col][str_val] = 1
                 else:
-                    centroid[i] = sums[i] / n
-        return centroid
+                    # Otherwise, if our column is numeric, we just continue building a sum for that column.
+                    sums[attr_col] += obs[attr_col]
+
+        # We now calculate the "means", which is an average for numeric columns and the mode for string columns.
+        mean = [0 for i in range(num_cols)]
+        # We again want to just iterate over the attribute columns instead of all the columns
+        for attr_col in attr_cols:
+            # If our attribute column is string-valued...
+            if attr_col in str_attr_cols:
+                # ...we find the most frequent value...
+                freq = freqs[attr_col]
+                max_attr_val = None
+                for attr_val in freq:
+                    if max_attr_val is None or freq[attr_val] > freq[max_attr_val]:
+                        max_attr_val = attr_val
+                # ...and set the mean for this column to be that value.
+                mean[attr_col] = max_attr_val
+            else:
+                # Otherwise, if our column is numeric, we divide the sum by the size of the cluster to get an average.
+                mean[attr_col] = sums[attr_col] / len(cluster)
+
+        # Lastly, we return this mean.
+        return mean
 
     def run(self, example):
         # Placeholder for future return value.
---
+++ src/test.py
@@ -0,0 +1,28 @@
+# This file can be used for testing out simplified data sets.
+
+import src.datasets.data_set as ds
+import src.algorithms.k_means as kmeans
+
+
+def get_three_clusters_data():
+    three_clusters_data = ds.DataSet("../data/test/three_clusters.data", 2, [0,1])
+    three_clusters_data.convert_to_float([0, 1])
+    three_clusters_data.normalize_z_score([0, 1])
+    return three_clusters_data
+
+
+def test_kmeans():
+    three_clusters_data = get_three_clusters_data()
+
+    km = kmeans.KMeans(three_clusters_data, 3)
+    # For verification, each class should have its own cluster:
+    print("Clusters from kmeans:")
+    for count, cluster in enumerate(km.clusters, 1):
+        print(str(count) + ": " + str(cluster))
+
+
+def main():
+    test_kmeans()
+
+
+main()
