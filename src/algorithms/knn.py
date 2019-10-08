0758347326e69ddc3f7d18cabe6b4657dad401cc
Alan Tong <alantong1234@hotmail.com>
2019-10-08 11:19:07 -0600

    updated knn

--- src/algorithms/knn.py
+++ src/algorithms/knn.py
@@ -14,7 +14,12 @@ class KNN:
         attribute_columns = self.training_data.attr_cols
         print(attribute_columns)
         for groups in data:
-            self.distance.append(self.training_data.distance(groups, test_point))
+            transformed_array = [groups[i] for i in attribute_columns]
+            test_point = [test_point[i] for i in attribute_columns]
+            self.distance.append(self.training_data.distance(transformed_array, test_point))
+        self.find_k_smallest(self.distance, self.k)
+        print("SORTED: ")
+        print(sorted(self.distance))
 
 
         ## Specific to abalone data ----------------
@@ -34,10 +39,15 @@ class KNN:
         # Placeholder for future return value.
         return {'first_class': 1/5, 'second_class': 2/5, 'third_class': 2/5}
 
-    def find_k_smallest(self, arr_distance, k):
+
+    # TODO This function does not behave the way it should be. Even with normal numbers, the function does not return
+    #   the kth_smallest array.
+    def find_k_smallest(self, arr_distance, kth):
+        list = [9,4,2,4,1,3,6,3,7,3]
+        k = 4
         k_smallest = list[0:k]
         largest = max(k_smallest)
-        left_over = list[k:0]
+        left_over = list[k:]
         for item in left_over:
             if item < largest:
                 # We find the largest element in k_smallest now...
@@ -49,8 +59,6 @@ class KNN:
                 k_smallest[max_index] = item
                 # this can be optimized more by doing it in the previous loop, but for simplicity we can just use min.
                 largest = max(k_smallest)
-
-
         print("k smallest: ")
         print(k_smallest)
 
