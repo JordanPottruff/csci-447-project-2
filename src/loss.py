7047368a9bfc08ca1ca5ccd387770e7d608462ac
Jordan Pottruff <jordanpottruff@gmail.com>
2019-10-09 13:24:27 -0600

    Update experimental design in driver.

--- src/loss.py
+++ src/loss.py
@@ -27,6 +27,22 @@ def calc_hinge(results):
     return hinge_sum / len(results)
 
 
+def calc_mse(results):
+    mse_sum = 0
+    for result in results:
+        expected_val = result['expected']
+        actual_val = get_expected_value(result['actual'])
+        mse_sum += (expected_val - actual_val)**2
+    return mse_sum / len(results)
+
+
+def get_expected_value(distribution):
+    weighted_sum = 0
+    for value in distribution:
+        weighted_sum += value * distribution[value]
+    return weighted_sum
+
+
 def get_highest_class(classes):
     highest_cls = None
     for cls in classes:
