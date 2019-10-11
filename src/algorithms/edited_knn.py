6e856083413b0061d0d9bb07125c32e6703e37e3
jordanpottruff <jordanpottruff@gmail.com>
2019-10-10 22:50:07 -0600

    Updated edited.

--- src/algorithms/edited_knn.py
+++ src/algorithms/edited_knn.py
@@ -2,28 +2,43 @@
 from src.algorithms.knn import KNN
 import src.util as util
 import src.datasets.data_set as ds
+import src.loss as loss
 
 
 class EditedKNN(KNN):
     def __init__(self, training_data, k):
         super().__init__(training_data, k)
-        self.training_data = training_data
+        partitions = training_data.partition(.20)
+        self.training_data = partitions[1]
+        self.validation_data = partitions[0]
         self.removed_data_set = []
-        self.training_data = self.find_edited_data()
+        self.find_edited_data()
         self.edited_data_size = self.get_edited_data_size()
 
     # Updates the edit_training_data variable to the edited data_set, that is the data_set with unnecessary
     # vectors removed
-    def find_edited_data(self) -> list:
-        edited_data_set = self.training_data.get_data()
-        # print("Original Data_Set Length: " + str(len(edited_data_set)))
-        for index, data in enumerate(self.training_data.get_data()):
-            if util.get_highest_class(self.run(data)) == data[self.training_data.class_col]:
-                self.removed_data_set.append(data)  # Add vector into the removed data list
-                edited_data_set.pop(index)  # Removed vector from the training data
-        self.edited_data_size = len(edited_data_set)
-        # print("Edited Data_Set Length: " + str(len(edited_data_set)))
-        return ds.DataSet(edited_data_set, self.training_data.class_col, self.training_data.attr_cols)
+    def find_edited_data(self):
+        while True:
+            previous_training = self.training_data.data.copy()
+            previous_accuracy = self.validate_accuracy()
+
+            new_training = []
+            for index, data in enumerate(self.training_data.get_data()):
+                if util.get_highest_class(self.run(data)) == data[self.training_data.class_col]:
+                    self.removed_data_set.append(data)  # Add vector into the removed data list
+                else:
+                    new_training.append(self.training_data.data[index])
+
+            self.training_data.data = new_training
+            new_accuracy = self.validate_accuracy()
+
+            if new_accuracy >= previous_accuracy:
+                self.training_data = ds.DataSet(new_training, self.training_data.class_col, self.training_data.attr_cols)
+            else:
+                break
+
+    def validate_accuracy(self):
+        return loss.calc_accuracy([{"actual": self.run(example), "expected": example[self.validation_data.class_col]} for example in self.validation_data.data]) / len(self.validation_data.data)
 
     # Returns the length of the edited_data_set
     def get_edited_data_size(self):
